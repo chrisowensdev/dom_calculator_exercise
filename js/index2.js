ad049de1c4cf2bb9ed595f31e4520c2b65780fd0
Chris Owens <guitarrockstarco@gmail.com>
2020-08-20 12:52:02 -0400

    Updated the index2.js

--- js/index2.js
+++ js/index2.js
@@ -9,20 +9,38 @@ const input = document.getElementById('input'),
 
 let resultDisplayed = false;
 
+let numberOperatorsArray = [];
+
 numbers.forEach(function (number) {
     number.addEventListener('click', function () {
         input.innerHTML += this.innerHTML;
+        numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
     });
 });
 
 operators.forEach(function (operator) {
     operator.addEventListener('click', function () {
         input.innerHTML += this.innerHTML;
+        numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
     });
 });
 
 result.addEventListener('click', function () {
-    alert("DO THE MATHS!!");
+    console.log("The numbers operators array is currently ", numberOperatorsArray);
+    let numbersStringHolder = '';
+    let equalFunctionArray = [];
+    for (let char of numberOperatorsArray) {
+        console.log('the char is:', char)
+        let numReg = /\d/;
+        if (numReg.test(char) || char === '.') {
+            numbersStringHolder += char;
+        } else {
+            equalFunctionArray = [...equalFunctionArray, Number(numbersStringHolder), char];
+            numbersStringHolder = '';
+        }
+    }
+    equalFunctionArray = [...equalFunctionArray, Number(numbersStringHolder)];
+    console.log('The array that is inside of the euqals function', equalFunctionArray);
 })
 
 clear.addEventListener('click', function () {
